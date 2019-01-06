4c60dc56ebcacc1fdf508197baacacb3a7d20484
Niraj Basnet <nirajbasnet2008@gmail.com>
2019-01-06 00:11:19 -0800

    initial commit including world file and learner code

---
+++ Learner.py
@@ -0,0 +1,169 @@
+import World
+import threading
+import time
+import random
+import math
+
+
+class Qlearning:
+    def __init__(self):
+        self.discount=0.90
+        self.epsilon=0.1
+        self.alpha = 0.2
+        self.actions = World.actions
+        self.states = []
+        self.Q = {}
+        self.bias=1.0
+        self.weights=[0.1,0.1]
+        self.weights_sp=[0.1,0.1]
+        self.train_episodes=0
+        for i in range(World.x):
+            for j in range(World.y):
+                self.states.append((i, j))
+
+        for state in self.states:
+            temp = {}
+            for action in self.actions:
+                temp[action] = 0.1
+                World.set_cell_score(state, action, temp[action])
+            self.Q[state] = temp
+
+        for (i, j) in World.targets:
+            for action in self.actions:
+                self.Q[(i, j)][action] = 1                                 #score for reaching the target
+                World.set_cell_score((i, j), action, 1)
+
+
+    def do_action(self,action):
+        s = World.player[0]
+        r = -World.score
+        World.try_move(World.characters[0], action)
+        s2 = World.player[0]
+        r += World.score
+        return s, action, r, s2
+
+    def do_action_sp(self,action):
+        sp = World.player[0]
+        r = -World.score_sp
+        World.try_move(World.characters[1], action)
+        sp2 = World.player[1]
+        r += World.score_sp
+        return sp, action, r, sp2
+
+    def valid_actions(self,s):
+        valid_acts=[]
+        for a in World.actions:
+            dx,dy=World.map_action_commands(a)
+            newx,newy=s[0]+dx,s[1]+dy
+            if newx<World.x and newx>=0:
+                if newy<World.y and newy>=0:
+                    valid_acts.append(a)
+        return valid_acts
+
+    def manhattandistance(self,s2,s):
+        delta = (s2[0] - s[0], s2[1] - s[1])
+        dis=abs(delta[0])+abs(delta[1])
+        if dis==0:
+            return 1.0
+        else:
+            return (1.0/dis)
+
+    def get_qvalue(self,s,a):
+        dx, dy = World.map_action_commands(a)
+        new_s=(s[0]+dx,s[1]+dy)
+        dist=self.manhattandistance(World.targets[0],new_s)
+        sum_feature=self.weights[0]*self.bias+self.weights[1]*dist
+        return sum_feature
+
+    def get_qvalue_sp(self,s,a):
+        dx, dy = World.map_action_commands(a)
+        new_s=(s[0]+dx,s[1]+dy)
+        dist=self.manhattandistance(World.targets[0],new_s)
+        sum_feature=self.weights_sp[0]*self.bias+self.weights_sp[1]*dist
+        return sum_feature
+
+
+    def max_Q(self,s):
+        maxQValue = float('-inf')
+        for a in World.actions:
+            value=self.get_qvalue(s,a)
+            if value>maxQValue:
+                maxQValue=value
+                maxact=a
+        return maxact,maxQValue
+
+    def max_Q_sp(self,s):
+        maxQValue = float('-inf')
+        for a in World.actions:
+            value=self.get_qvalue_sp(s,a)
+            if value>maxQValue:
+                maxQValue=value
+                maxact=a
+        return maxact,maxQValue
+
+
+    def inc_Q(self,s, a, alpha, inc):
+        self.Q[s][a] *= 1 - alpha
+        self.Q[s][a] += alpha * inc
+        World.set_cell_score(s, a, self.Q[s][a])
+
+    def find_best_action(self,state):
+        if(random.random()<self.epsilon and self.train_episodes<100):
+            act=random.choice(World.actions)
+        else:
+            act,val=self.max_Q(state)
+        return act
+
+    def find_best_action_sp(self,state):
+        if(random.random()<self.epsilon and self.train_episodes<100):
+            act=random.choice(World.actions)
+        else:
+            act,val=self.max_Q_sp(state)
+        return act
+
+
+    def run(self):
+        time.sleep(1)
+        t = 1
+        while True:
+            # Choosing the best action at current state and executing it
+            s = World.player[0]
+            sp= World.player[1]
+            World.try_move(World.characters[2], random.choice(World.actions))
+            max_act=self.find_best_action(s)
+            max_act_sp=self.find_best_action_sp(sp)
+            s, a, r, s2 = self.do_action(max_act)
+            sp,a_sp,r_sp,sp2=self.do_action_sp(max_act_sp)
+
+            total_reward=r+r_sp
+            if self.train_episodes<100:
+                # Update Q
+                max_act, max_val = self.max_Q(s2)
+                difference = (total_reward + self.discount * max_val) - self.get_qvalue(s,a)
+                self.weights[0] += self.alpha * difference * 1.0       #for bias feature
+                self.weights[1] += self.alpha * difference * self.manhattandistance(World.targets[0],s)   #for distance feature
+
+                max_act_sp, max_val_sp = self.max_Q_sp(sp2)
+                difference = (total_reward + self.discount * max_val_sp) - self.get_qvalue_sp(sp2, a_sp)
+                self.weights_sp[0] += self.alpha * difference * 1.0  # for bias feature
+                self.weights_sp[1] += self.alpha * difference * self.manhattandistance(World.targets[0],
+                                                                                    sp2)  # for distance feature
+
+
+
+            t += 1.0
+            if World.has_restarted():
+                self.train_episodes += 1
+                World.restart_game()
+                time.sleep(0.01)
+                t = 0.3
+
+
+            time.sleep(0.1)
+
+if __name__== "__main__":
+    qlearning = Qlearning()
+    t = threading.Thread(target=qlearning.run)
+    t.daemon = True
+    t.start()
+    World.start_game()
---
+++ World.py
@@ -0,0 +1,217 @@
+import random
+from tkinter import *
+master = Tk()
+
+triangle_size = 0.1
+cell_score_min = -0.2
+cell_score_max = 0.2
+Width = 100
+(x, y) = (10, 5)
+actions = ["up", "down", "left", "right"]
+characters=["player1","player2","target"]
+board = Canvas(master, width=x*Width, height=y*Width)
+player_home=[(4,2),(1,2)]
+player = [(4, 2),(1,2)]
+score = 1
+score_sp=1
+restart = False
+walk_reward = -0.05
+target_home=[(9,3)]
+targets= [(9, 3)]
+cell_scores = {}
+episode=0
+moves=0
+moves_sp=0
+sum_moves=0
+
+def create_triangle(i, j, action):
+    if action == actions[0]:
+        return board.create_polygon((i+0.5-triangle_size)*Width, (j+triangle_size)*Width,
+                                    (i+0.5+triangle_size)*Width, (j+triangle_size)*Width,
+                                    (i+0.5)*Width, j*Width,
+                                    fill="white", width=1)
+    elif action == actions[1]:
+        return board.create_polygon((i+0.5-triangle_size)*Width, (j+1-triangle_size)*Width,
+                                    (i+0.5+triangle_size)*Width, (j+1-triangle_size)*Width,
+                                    (i+0.5)*Width, (j+1)*Width,
+                                    fill="white", width=1)
+    elif action == actions[2]:
+        return board.create_polygon((i+triangle_size)*Width, (j+0.5-triangle_size)*Width,
+                                    (i+triangle_size)*Width, (j+0.5+triangle_size)*Width,
+                                    i*Width, (j+0.5)*Width,
+                                    fill="white", width=1)
+    elif action == actions[3]:
+        return board.create_polygon((i+1-triangle_size)*Width, (j+0.5-triangle_size)*Width,
+                                    (i+1-triangle_size)*Width, (j+0.5+triangle_size)*Width,
+                                    (i+1)*Width, (j+0.5)*Width,
+                                    fill="white", width=1)
+
+
+def render_grid():
+    global specials, walls, Width, x, y, player
+    for i in range(x):
+        for j in range(y):
+            board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="white", width=1)
+            temp = {}
+            for action in actions:
+                temp[action] = create_triangle(i, j, action)
+            cell_scores[(i,j)] = temp
+
+render_grid()
+
+
+def set_cell_score(state, action, val):
+    global cell_score_min, cell_score_max
+    triangle = cell_scores[state][action]
+    green_dec = int(min(255, max(0, (val - cell_score_min) * 255.0 / (cell_score_max - cell_score_min))))
+    green = hex(green_dec)[2:]
+    red = hex(255-green_dec)[2:]
+    if len(red) == 1:
+        red += "0"
+    if len(green) == 1:
+        green += "0"
+    color = "#" + red + green + "00"
+    board.itemconfigure(triangle, fill=color)
+
+
+def map_action_commands(action_cmd):
+    if action_cmd=="up":
+        return 0,-1
+    elif action_cmd=="down":
+        return 0,1
+    elif action_cmd=="right":
+        return 1,0
+    elif action_cmd=="left":
+        return -1,0
+    else:
+        return 0,0
+
+
+def try_move(agent, action_cmd):
+    global player,episode,targets,characters, score, walk_reward, me,me2,target, restart , moves,moves_sp,score_sp,sum_moves
+    player_id=0
+    target_id=1
+
+    if restart == True:
+        restart_game()
+    if agent == characters[0]:
+        player_id = 0
+        display_object=me
+        old_x=player[0][0]
+        old_y=player[0][1]
+        moves += 1
+        score += walk_reward
+    elif agent == characters[1]:
+        player_id = 1
+        display_object = me2
+        old_x = player[1][0]
+        old_y = player[1][1]
+        moves_sp+= 1
+        score_sp += walk_reward
+    elif agent == characters[2]:
+        target_id = 0
+        display_object = target
+        old_x = targets[0][0]
+        old_y = targets[0][1]
+    dx,dy=map_action_commands(action_cmd)
+    new_x = old_x + dx
+    new_y = old_y + dy
+
+    if (new_x >= 0) and (new_x < x) and (new_y >= 0) and (new_y < y):
+        board.coords(display_object, new_x*Width+Width*2/10, new_y*Width+Width*2/10, new_x*Width+Width*8/10, new_y*Width+Width*8/10)
+        if target_id==0:
+            set_cell_score(targets[target_id], action_cmd, 0.0)
+            targets[target_id]=(new_x,new_y)
+            set_cell_score(targets[target_id], action_cmd, 1)
+            return
+        else:
+            player[player_id] = (new_x, new_y)
+    for (i, j) in targets:
+
+        if player[0][0] == i and player[0][1] == j:
+            episode += 1
+            score -= walk_reward
+            score += 1
+            sum_moves+=moves
+            if score > 0:
+                print("epi:", episode, " Success! score: ", score, "moves:",sum_moves)
+            else:
+                print("Fail! score: ", score)
+            moves=0
+            moves_sp=0
+            restart = True
+            return
+
+        if player[1][0] == i and player[1][1] == j:
+            # episode += 1
+            score_sp -= walk_reward
+            score_sp += 1
+            if score_sp > 0:
+                print("")
+                #print("epi:", episode, " Success! score_sp: ", score_sp, "moves:",moves_sp/2.0)
+            else:
+                print("Fail! score: ", score_sp)
+            moves_sp=0
+            moves=0
+            restart = True
+            return
+
+def call_up(event):
+    try_move(characters[0],actions[0])
+
+def call_down(event):
+    try_move(characters[0],actions[1])
+
+def call_left(event):
+    try_move(characters[0],actions[2])
+
+def call_right(event):
+    try_move(characters[0],actions[3])
+
+def restart_game():
+    global player, score, me,me2,target, restart
+    # player[0] = player_home[0]
+    player[0]=(random.randint(0,x),random.randint(0,y))
+
+    player_home[0]=player[0]
+    #player[1]=(random.randint(0,x),random.randint(0,y))
+    player[1]=player[0]
+    player_home[1] = player[1]
+
+    print("home", player[0])
+    print("home2", player[1])
+    targets[0]=target_home[0]
+    if (player[0]==targets[0]):
+        restart_game()
+    score = 1
+    score_sp=1
+    board.coords(me, player_home[0][0]*Width+Width*2/10, player_home[0][1]*Width+Width*2/10, player_home[0][0]*Width+Width*8/10, player_home[0][1]*Width+Width*8/10)
+    board.coords(me2, player_home[1][0] * Width + Width * 2 / 10, player_home[1][1] * Width + Width * 2 / 10,
+                 player_home[1][0] * Width + Width * 8 / 10, player_home[1][1] * Width + Width * 8 / 10)
+    board.coords(target, target_home[0][0] * Width + Width * 2 / 10, target_home[0][1] * Width + Width * 2 / 10,
+                 target_home[0][0] * Width + Width * 8 / 10, target_home[0][1] * Width + Width * 8 / 10)
+    restart = False
+
+
+
+def has_restarted():
+    return restart
+
+master.bind("<Up>", call_up)
+master.bind("<Down>", call_down)
+master.bind("<Right>", call_right)
+master.bind("<Left>", call_left)
+
+me = board.create_rectangle(player[0][0]*Width+Width*2/10, player[0][1]*Width+Width*2/10,
+                            player[0][0]*Width+Width*8/10, player[0][1]*Width+Width*8/10, fill="blue", width=1, tag="me")
+me2 = board.create_rectangle(player[1][0]*Width+Width*2/10, player[1][1]*Width+Width*2/10,
+                            player[1][0]*Width+Width*8/10, player[1][1]*Width+Width*8/10, fill="orange", width=1, tag="me2")
+target = board.create_rectangle(targets[0][0]*Width+Width*2/10, targets[0][1]*Width+Width*2/10,
+                                targets[0][0]*Width+Width*8/10, targets[0][1]*Width+Width*8/10, fill="red", width=1, tag="target")
+
+
+board.grid(row=0, column=0)
+
+
+def start_game():
+    master.mainloop()
